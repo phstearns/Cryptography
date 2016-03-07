3ee7456d30deec2f7ea55543929d51bcd8523b72
phstearns <phstearns13@gmail.com>
2016-03-07 14:44:25 -0500

    Updated from Brython Server: 3/7/2016 2:44:27 PM

--- cryptography.py
+++ cryptography.py
@@ -51,11 +51,11 @@ while quit == False:
             trun = key[0:(m%k)]
             newkey = count + trun
         elif k>m:
-            newkey = key[0:k] 
+            newkey = key[0:m] 
             print(newkey, message)
-        for x in range (0,k):
+        for x in range (0,m):
             let.append(associations.find(message[x]))
-        for y in range (0,k):
+        for y in range (0,m):
             kelt.append(associations.find(newkey[y]))
         for c in range (0,len(kelt)):
             r = (let[c] + kelt[c]) % 85
