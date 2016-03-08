574a932f8d4d4426b230823e66fc836ace8d34cb
phstearns <phstearns13@gmail.com>
2016-03-08 14:20:23 -0500

    Updated from Brython Server: 3/8/2016 2:20:24 PM

--- cryptography.py
+++ cryptography.py
@@ -55,7 +55,7 @@ while quit == False:
             print(newkey, message)
         for x in range (0,m):
             let.append(associations.find(message[x]))
-        for y in range (0,[-m]):
+        for y in range (0,-m):
             kelt.append(associations.find(newkey[y]))
         for c in range (0,len(kelt)):
             r = (let[c] - kelt[c]) % 85
